95292ff7feec47baefc90eabbb979691cd469936
sophiekim <sophiekim@sophiekimui-MacBookPro.local>
2019-04-13 21:51:30 +1200

    split add

--- js/script.js
+++ js/script.js
@@ -7,11 +7,25 @@ $('#menuIcon').click(function(){
 $(".item").click(function(){
   $(this).find(">:first-child").toggle();
 });
+//
+// $('body').click(function(e) {
+//   if ($(e.target).closest('.popup').length === 0) {
+//     $("#payWay").hide();
+//   }
+// });
 
-$(".tipBtn").click(function(){
+$("#tip").click(function(){
   $("#tipBox").toggle();
+
 });
 
-// $(".payBtn").click(function(){
-//   $("#payWay").toggle();
-// })
+$(".addBtn").click(function(){
+	$(".lastqt").append('<div class="qty">1</div>')
+  $(".lastnm").append('<div class="itemName">Tip</div>')
+  $(".lastpr").append('<div class="price">$ 5.00</div>')
+
+});
+
+$("#pay").click(function(){
+  $("#payWay").toggle();
+});
