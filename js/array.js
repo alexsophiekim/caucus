03f01031a27371665f33ac62e8e9abaa29de8a6b
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-11-06 21:02:01 +1300

    cafe.html/scss edited

---
+++ js/array.js
@@ -0,0 +1,110 @@
+var menu = [
+  {
+    title : 'entree',
+    lists: [
+      {
+        id:1,
+        name: 'Bread and Dip',
+        price: 12,
+        sort: [],
+        descript: 'artisan bread widh olive oil and selection of dips'
+      },
+      {
+        id:2,
+        name: 'Salmon gravlax',
+        price: 14,
+        sort: ['GF'],
+        descript: 'capers, horseradish cream, pickled cucumber, alfalfa salad'
+      },
+      {
+        id:3,
+        name: 'Oriental chicken karaagep',
+        price: 13,
+        sort: ['GF'],
+        descript: 'sauteed, mung bean sprout, oriental fried chicken thigh balls, edamame, Japanese mayo'
+      },
+      {
+        id:4,
+        name: 'Truffle goat cheese crostini',
+        price: 14,
+        sort: ['V','N'],
+        descript: 'coriander, spiced beetroot, truffle paste, roast pistachio and grapes'
+      },
+      {
+        id:5,
+        name: 'Fried calamari rocket salad',
+        price: 13,
+        sort: ['V','GF'],
+        descript: 'fried calamari, picked red cabbage, rocket, roasted cashews, YUZU dressing, with chipotle sause'
+      },
+      {
+        id:6,
+        name: 'BBQ pork rib pot pie',
+        price: 13,
+        sort: [],
+        descript: 'bourbon bbq sause with pulled and braised pork rib, served with beer battered babt gherkin, whole grain mustard and roast bell pepper'
+      },
+    ]
+  },
+  {
+    title : 'main',
+    lists: [
+      {
+        id:7,
+        name: 'Line caught fish',
+        price: 34,
+        sort: ['N'],
+        descript: 'shrimp pompom, buttered flat bean, cloudy bay clam, seaweed vinaigrette salad, poached macadamia, spinach and parmesan pesto'
+      },
+      {
+        id:8,
+        name: 'Prosciutto wrapped chicken breast',
+        price: 33,
+        sort: ['N','GF'],
+        descript: 'spinach, baked ricotta, pumpkin and chorizo filled chicken breast, creamy potato gnocchi, broccolini, pine nuts and red wine jus'
+      },
+      {
+        id:9,
+        name: 'OP Rib 450g',
+        price: 46,
+        sort: [],
+        descript: 'carrot and ginger puree, potato gratin, baby carrots, black garlic puree, braised beef cheek vol au vent and vine tomato'
+      },
+      {
+        id:10,
+        name: 'Rib Eye 200g',
+        price: 36,
+        sort: [],
+        descript: 'carrot and ginger puree, potato gratin, baby carrots, black garlic puree, braised beef cheek vol au vent and vine tomato'
+      },
+      {
+        id:11,
+        name: 'Eye Fillet 180g',
+        price: 38,
+        sort: [],
+        descript: 'carrot and ginger puree, potato gratin, baby carrots, black garlic puree, braised beef cheek vol au vent and vine tomato'
+      },
+      {
+        id:12,
+        name: 'Beef short rib',
+        price: 38,
+        sort: ['GF'],
+        descript: '8hour braised beef short rib, confit garlic, green beans, creamy mash potato and served with red wine jus'
+      },
+      {
+        id:13,
+        name: 'Lamb rump',
+        price: 35,
+        sort: [],
+        descript: 'pomegranate glazed orecchiette, creamy kumara puree, red wine poached cherry, grilled courgette, served with minted red wine jus'
+      },
+      {
+        id:14,
+        name: 'Crumbed ratatouille',
+        price: 29,
+        sort: [],
+        descript: 'beetroot creme fraiche, parmesan crisp, poached pear and fig jam, orange polenta chip and cheesy garlic bread'
+      }
+    ]
+  }
+]
